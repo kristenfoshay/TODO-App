5bb21011952ebe2fff41d649dd9edd9fc3cab6f6
kristenfoshay <88470531+kristenfoshay@users.noreply.github.com>
2021-08-10 20:09:27 -0400

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,34 +1,42 @@
 let array = JSON.parse(localStorage.getItem('array')) || [];
 
-function storeinlocalStorage (array){
-    localStorage.setItem('array', JSON.stringify(array));
+function storeinlocalStorage(array) {
+	localStorage.setItem('array', JSON.stringify(array));
 }
 
-function getInputValue(event){
-    event.preventDefault();
-    let inputVal = document.getElementById("myInput").value;
-    let source = document.getElementById("lu");
-   
-    let node = document.createElement("li");
-  
-    let newli = source.appendChild(node);
-   newli.textContent += inputVal;
-   let item = {name:inputVal};
-   array.push(item);
-   storeinlocalStorage(array);
-   
-   let button = document.createElement('button');
-button.textContent = "Remove";
+function getInputValue() {
+	
+	let inputVal = document.getElementById('myInput').value;
+	if (inputVal.length == 0){
+		alert("PLEASE ENTER YOUR TO DO IN THE TEXT FIELD");
+		return;
+	}
+	let source = document.getElementById('lu');
 
-newli.appendChild(button);}
+	let node = document.createElement('li');
 
-let el = document.getElementById("btn");
-el.addEventListener("click", getInputValue, false); 
+	let newli = source.appendChild(node);
+	newli.textContent += inputVal;
+	let item = { name: inputVal };
+	array.push(item);
+	storeinlocalStorage(array);
+
+	let button = document.createElement('button');
+	button.textContent = 'Remove';
+
+	newli.appendChild(button);
+}
+
+let submitBtn = document.getElementById('btn');
+submitBtn.addEventListener('click', getInputValue, false);
 
 let parent = document.getElementById('lu');
 
-parent.addEventListener("click", function(event) {
-if (event.target.tagName === "BUTTON") {
-event.target.parentElement.remove();
-}else if (event.target.tagName === "LI"){event.target.style.textDecoration = "line-through";}
+parent.addEventListener('click', function(event) {
+	if (event.target.tagName === 'BUTTON') {
+		event.target.parentElement.remove();
+	}
+	else if (event.target.tagName === 'LI') {
+		event.target.style.textDecoration = 'line-through';
+	}
 });
